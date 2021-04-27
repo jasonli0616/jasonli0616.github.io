df2e87c981425075ac20b8209c76a3ac20714950
Jason Li <jasonli.0616@outlook.com>
2021-04-27 09:13:54 -0400

    Add projects section

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("pyinstallergui-github-btn").onclick = function() {
+    window.open("https://github.com/HDSB-GWS-ProgrammingClub/PyInstaller-GUI", "_blank");
+}
+
+document.getElementById("bscode-github-btn").onclick = function() {
+    window.open("https://github.com/jasonli0616/BSCode", "_blank");
+}
+
+document.getElementById("pyshellbot-github-btn").onclick = function() {
+    window.open("https://github.com/jasonli0616/PyShell-Discord-Bot", "_blank");
+}
+
