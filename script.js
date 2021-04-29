56b644874b2951054299886b25552ecb843b3c3f
Jason Li <jasonli.0616@outlook.com>
2021-04-29 12:12:51 -0400

    Fix screen size alignment

--- script.js
+++ script.js
@@ -4,34 +4,86 @@ made by: Jason Li
 */
 
 // Adjust intro for screen size
-let screen_width = screen.availWidth
-let intro_section = document.getElementById("intro-section")
-if (screen_width <= 1005) {
-    intro_section.innerHTML = `
-        <div class="py-5" id="intro_section">
-            <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col">
-                            <img src="files/jl_logo.png" alt="" style="max-width: 18rem;">
-                        </div>
-                        <div class="col">
-                            <h1 class="display-1 text-light">Jason Li</h1>
-                            <h5 class="display-5 text-light">Aspiring smart person</h3>
-                            <p class="lead text-light" id="intro-text">
-                                I am a high school student in Canada interested in programming with experience in Python, HTML, and CSS. To see my projects or to learn more about me, click the buttons below.
-                            </p>
-                            <p>
-                                <button class="btn btn-outline-info" onclick="scrollToSection('projects')">Projects</button>
-                                <button class="btn btn-outline-light" onclick="scrollToSection('about-me')">About me</button>
-                                <button class="btn btn-outline-light" onclick="scrollToSection('contact')">Contact</button>
-                            </p>
+function adjustIntroForScreenSize() {
+    let screen_width = screen.availWidth
+    let intro_section = document.getElementById("intro-section")
+    if (screen_width <= 770 && screen_width >= 450) {
+        intro_section.innerHTML = `
+            <div class="py-5" id="intro-section">
+                <div class="container">
+                    <img src="files/jl_logo.png" alt="" style="max-width: 18rem;">
+                        <h1 class="display-1 text-light">Jason Li</h1>
+                        <h5 class="display-5 text-light">Aspiring smart person</h3>
+                        <p class="lead text-light" id="intro-text">
+                            I am a high school student in Canada interested in programming with experience in Python, HTML, and CSS. To see my projects or to learn more about me, click the buttons below.
+                        </p>
+                        <p>
+                            <button class="btn btn-outline-info" onclick="scrollToSection('projects')">Projects</button>
+                            <button class="btn btn-outline-light" onclick="scrollToSection('about-me')">About me</button>
+                            <button class="btn btn-outline-light" onclick="scrollToSection('contact')">Contact</button>
+                        </p>
+                </div>
+            </div>
+        `
+    } else if (screen_width <= 1005) {
+        intro_section.innerHTML = `
+            <div class="py-5" id="intro_section">
+                <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col">
+                                <img src="files/jl_logo.png" alt="" style="max-width: 18rem;">
+                            </div>
+                            <div class="col">
+                                <h1 class="display-1 text-light">Jason Li</h1>
+                                <h5 class="display-5 text-light">Aspiring smart person</h3>
+                                <p class="lead text-light" id="intro-text">
+                                    I am a high school student in Canada interested in programming with experience in Python, HTML, and CSS. To see my projects or to learn more about me, click the buttons below.
+                                </p>
+                                <p>
+                                    <button class="btn btn-outline-info" onclick="scrollToSection('projects')">Projects</button>
+                                    <button class="btn btn-outline-light" onclick="scrollToSection('about-me')">About me</button>
+                                    <button class="btn btn-outline-light" onclick="scrollToSection('contact')">Contact</button>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    `
+        `
+    } else {
+        intro_section.innerHTML = `
+            <div class="py-5" id="intro-section">
+                <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-4">
+                                <img src="files/jl_logo.png" alt="" style="max-width: 18rem;">
+                            </div>
+                            <div class="col-5">
+                                <h1 class="display-1 text-light">Jason Li</h1>
+                                <h5 class="display-5 text-light">Aspiring smart person</h3>
+                                <p class="lead text-light" id="intro-text">
+                                    I am a high school student in Canada interested in programming with experience in Python, HTML, and CSS. To see my projects or to learn more about me, click the buttons below.
+                                </p>
+                                <p>
+                                    <button class="btn btn-outline-info" onclick="scrollToSection('projects')">Projects</button>
+                                    <button class="btn btn-outline-light" onclick="scrollToSection('about-me')">About me</button>
+                                    <button class="btn btn-outline-light" onclick="scrollToSection('contact')">Contact</button>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }
 }
+adjustIntroForScreenSize()
+
+
+// Adjust intro size on window resize event
+window.addEventListener('resize', function(event) {
+    adjustIntroForScreenSize()
+}, true);
 
 
 // Open GitHub on click
